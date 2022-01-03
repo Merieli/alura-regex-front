cdc75344944d73a105d2993783accd0c2d6a4773
Merieli Manzano <merieli.manzano@netzee.com.br>
2022-01-03 11:09:21 -0300

    feat: To simply test a REGEX

--- teste-regex.js
+++ teste-regex.js
@@ -1,7 +1,10 @@
 const alvo = '11a22b33c';
 const exp = /(\d\d)(\w)/g; //gera uma regex fixa, que tem como vantagem ser compilada e analisada quando ocorre o carregamento do navegador, ou seja, ela é mais performática
 let resultado = null;
-while (resultado = exp.exec(alvo)){ //enquanto o resultado existir irá continuar executando
-    console.log(resultado);
-    console.log(exp.lastIndex);
-}
+
+console.log(exp.test(alvo)); //testa se a REGEX existe dentro do alvo definido
+
+//while (resultado = exp.exec(alvo)){ //enquanto o resultado da Regex existir irá continuar executando
+//    console.log(resultado);
+//    console.log(exp.lastIndex);
+//}
