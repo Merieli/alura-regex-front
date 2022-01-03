739186560300ea160f008f6329855fc488f4edc0
Merieli Manzano <merieli.manzano@netzee.com.br>
2022-01-03 10:27:37 -0300

    feat: To run the Regex Engine as often as possible on the desired target

--- teste-regex.js
+++ teste-regex.js
@@ -1,5 +1,7 @@
 const alvo = '11a22b33c';
 const exp = /(\d\d)(\w)/g; //gera uma regex fixa, que tem como vantagem ser compilada e analisada quando ocorre o carregamento do navegador, ou seja, ela é mais performática
-let resultado = exp.exec(alvo);
-console.log(resultado);
-console.log(exp.lastIndex);
+let resultado = null;
+while (resultado = exp.exec(alvo)){ //enquanto o resultado existir irá continuar executando
+    console.log(resultado);
+    console.log(exp.lastIndex);
+}
