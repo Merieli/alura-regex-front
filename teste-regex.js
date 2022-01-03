b177a3798a61b2922a10fe845c337692e1f3b7d0
Merieli Manzano <merieli.manzano@netzee.com.br>
2022-01-03 10:23:47 -0300

    perf: generating a fixed regex that compiles when the browser loads

--- teste-regex.js
+++ teste-regex.js
@@ -1,5 +1,5 @@
 const alvo = '11a22b33c';
-const exp = new RegExp('(\\d\\d)(\\w)', 'g');
+const exp = /(\d\d)(\w)/g; //gera uma regex fixa, que tem como vantagem ser compilada e analisada quando ocorre o carregamento do navegador, ou seja, ela é mais performática
 let resultado = exp.exec(alvo);
 console.log(resultado);
 console.log(exp.lastIndex);
