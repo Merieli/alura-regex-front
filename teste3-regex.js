b786f935e2af532ef768a78863d23cbb3e2f5751
Merieli Manzano <merieli.manzano@netzee.com.br>
2022-01-04 07:49:35 -0300

    refactor: variables to create target and pattern

--- teste3-regex.js
+++ teste3-regex.js
@@ -1,5 +1,5 @@
 //Como dividir um texto que deveria ser um arquivo CSV (valores separados por vírgula), mas que na verdade tem valores separados por vírgula, hífen e ponto e vírgula sem um padrão:
-var arquivo = '100,200-150,200;20';
-var exp2 = /[,;-]/;
-var valores = arquivo.split(exp2);
+const arquivo = '100,200-150,200;20';
+const exp2 = /[,;-]/;
+let valores = arquivo.split(exp2);
 console.log(valores);
--- teste4-regex.js
+++ teste4-regex.js
@@ -1,6 +1,6 @@
-//Extrair os códigos contatenados que por padrão que começa com uma letra e termina com 1 ou mais dígitos, em um array para poder usar em verificações:
-var codigos = 'A121B12112C12212F12G01';
-var exp3 = /[A-Za-z]\d+/g;
-var codigosExtraidos = codigos.match(exp3);
+//Extrair os códigos concatenados que por padrão que começa com uma letra e termina com 1 ou mais dígitos, em um array para poder usar em verificações:
+const codigos = 'A121B12112C12212F12G01';
+const exp3 = /[A-Za-z]\d+/g;
+let codigosExtraidos = codigos.match(exp3);
 
 console.log(codigosExtraidos);
